bf7197fd24552130846be0c9e2f682a3398ce920
Vladislav1552 <vladiskav333@gmail.com>
2018-03-10 22:44:22 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+
+
+  module.exports = function getZerosCount(number, base)
+  
+  {
+
+      let zero = number;
+      let numberSystem = base;
+      let getZero = (zero) =>{
+      for (let i = 2; i <= base; i ++)
+      {
+
+          if (numberSystem % i == 0)
+          {
+          
+              let bazeNumper = 0;
+              let countOfPr = 0;
+              let whileCount = i;
+              let initNumber = number;
+              while (numberSystem % i == 0)
+              {
+                  bazeNumper += 1;
+                  numberSystem = numberSystem / i;
+                  
+              }
+              while ((number / whileCount) > 0)
+              {
+                  countOfPr += Math.floor(initNumber /whileCount);
+                  whileCount *= i;
+              }
+              let result = countOfPr / bazeNumper;
+              zero  = Math.min(zero, result);
+          }
+  
+      }
+      return Math.floor(zero);
+    }
+     return getZero(zero);
+  }
